fa9623fb22ff8ea48e05dde0aada1cadede71f52
NikhilAnap <nikanap19@gmail.com>
2023-09-12 15:29:05 +0530

    Initial Project

---
+++ SpringCrud_Git_01/src/main/java/com/cjc/main/SpringCrudGit01Application.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudGit01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudGit01Application.class, args);
+	}
+
+}
